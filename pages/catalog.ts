59da66fbbf782a998f809c6764b8a21332aa9e49
Nick <nick98_molo@icloud.com>
2022-07-11 15:36:31 +0300

    Added h/w.

---
+++ pages/catalog.ts
@@ -0,0 +1,43 @@
+import { By, until, WebElement } from "selenium-webdriver";
+import { driver } from "../constants/webdriver";
+
+async function clickCatalog(selector: string) {
+  const elementCatalog: WebElement = await driver.findElement(By.css(selector));
+  await driver.wait(until.elementIsVisible(elementCatalog));
+  await elementCatalog.click();
+}
+
+async function clickMidMenu(selector: string) {
+  const middleMenu = await driver.findElement(By.css(selector));
+  await middleMenu.click();
+}
+
+async function clickLeftSideMenu(chapterName: string) {
+  const leftMenu = await driver.findElement(
+    By.xpath(
+      `//div[contains(@class, 'list__aside-title') and normalize-space(text()) = '${chapterName}']`
+    )
+  );
+  await driver.wait(until.elementIsVisible(leftMenu));
+  await leftMenu.click();
+}
+
+async function clickProduct(selector: string) {
+  const product = await driver.findElement(By.css(selector));
+  await driver.wait(until.elementIsVisible(product));
+  await product.click();
+}
+
+async function getTitleProduct(selector: string) {
+  const titleProduct = await driver.findElement(By.css(selector));
+  await driver.wait(until.elementIsVisible(titleProduct));
+  return driver.getTitle();
+}
+
+export {
+  clickCatalog,
+  clickMidMenu,
+  clickLeftSideMenu,
+  clickProduct,
+  getTitleProduct,
+};
---
+++ specs/onliner.spec.ts
@@ -0,0 +1,125 @@
+import { driver } from "../constants/webdriver";
+import { BASE_URL } from "../constants/url";
+import { expect } from "chai";
+import { TITLES } from "../constants/titles";
+import {
+  SELECTORS_HOME_PAGE,
+  SELECTORS_MID_MENU,
+  SELECTORS_HEADERS_TITLES,
+  SELECTORS_PRODUCT,
+} from "../constants/selectors";
+import { LEFT_SIDE_BAR } from "../constants/leftSideBar";
+import {
+  clickCatalog,
+  clickLeftSideMenu,
+  clickMidMenu,
+  clickProduct,
+  getTitleProduct,
+} from "../pages/catalog";
+
+describe("Onliner tests", async () => {
+  before("Catalog page should be opened", async () => {
+    await driver.manage().window().maximize();
+    await driver.get(BASE_URL);
+
+    await clickCatalog(SELECTORS_HOME_PAGE.CATALOG);
+
+    const catalogTitle = await driver.getTitle();
+    expect(catalogTitle).to.eql(TITLES.CATALOG);
+  });
+
+  it("Tablets page should be opened", async () => {
+    try {
+      await clickMidMenu(SELECTORS_MID_MENU.ELECTRONICS);
+
+      await clickLeftSideMenu(LEFT_SIDE_BAR.TABLETS_EBOOKS);
+
+      await clickProduct(SELECTORS_PRODUCT.TABLETS);
+
+      const getTitleTablets = await getTitleProduct(
+        SELECTORS_HEADERS_TITLES.HEADERS_PRODUCT
+      );
+      expect(getTitleTablets).to.eql(TITLES.TABLETS);
+
+      await driver.getWindowHandle();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+
+  it("Gaming consoles page should be opened", async () => {
+    try {
+      await clickMidMenu(SELECTORS_MID_MENU.ELECTRONICS);
+
+      await clickLeftSideMenu(LEFT_SIDE_BAR.VIDEOGAMES);
+
+      await clickProduct(SELECTORS_PRODUCT.CONSOLE);
+
+      const getTitleConsole = await getTitleProduct(
+        SELECTORS_HEADERS_TITLES.HEADERS_PRODUCT
+      );
+      expect(getTitleConsole).to.eql(TITLES.CONSOLE);
+
+      await driver.getWindowHandle();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+
+  it("Batteries car page should be opened", async () => {
+    try {
+      await clickMidMenu(SELECTORS_MID_MENU.AUTO_MOTO);
+
+      await clickLeftSideMenu(LEFT_SIDE_BAR.SPARES);
+
+      await clickProduct(SELECTORS_PRODUCT.CAR_BATTERY);
+
+      const getTitleCarBattery = await getTitleProduct(
+        SELECTORS_HEADERS_TITLES.HEADERS_PRODUCT
+      );
+      expect(getTitleCarBattery).to.eql(TITLES.CAR_BATTERY);
+
+      await driver.getWindowHandle();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+
+  it("Office chairs page should be opened", async () => {
+    try {
+      await clickMidMenu(SELECTORS_MID_MENU.WORK_OFFICE);
+
+      await clickLeftSideMenu(LEFT_SIDE_BAR.OFFICE_FURNITURE);
+
+      await clickProduct(SELECTORS_PRODUCT.OFFICE_CHAIRS);
+
+      const getTitleOfficeChairs = await getTitleProduct(
+        SELECTORS_HEADERS_TITLES.HEADERS_PRODUCT
+      );
+      expect(getTitleOfficeChairs).to.eql(TITLES.OFFICE_CHAIRS);
+
+      await driver.getWindowHandle();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+
+  it("IP cameras page should be opened", async () => {
+    try {
+      await clickMidMenu(SELECTORS_MID_MENU.WORK_OFFICE);
+
+      await clickLeftSideMenu(LEFT_SIDE_BAR.VIDEO_SURVEILLANCE);
+
+      await clickProduct(SELECTORS_PRODUCT.IP_CAMERAS);
+
+      const getTitleIpCameras = await getTitleProduct(
+        SELECTORS_HEADERS_TITLES.HEADERS_PRODUCT
+      );
+      expect(getTitleIpCameras).to.eql(TITLES.IP_CAMERAS);
+
+      await driver.quit();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+});
